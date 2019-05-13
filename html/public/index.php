264479112e68345b0444d98d1a024b849886cbfd
Steven.Gomez <steve.gomez@biomerieux.com>
2019-05-13 13:37:19 -0500

    Preliminary structure created

---
+++ html/public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<title>Regency Estates Homeowners Association</title>
+		<meta charset="utf-8">
+	</head>
+
+	<body>
+		<h1>Regency Estates: Coming Soon</h1>
+	
+	</body>
+</html>
