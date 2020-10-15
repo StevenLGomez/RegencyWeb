662b0a74d6eb1199c70fe75d42120e0913bc953a
Steven Gomez <steve_gomez@usa.net>
2020-10-14 23:11:02 -0500

    More work on basic framework

--- html/public/staff/index.php
+++ html/public/staff/index.php
@@ -1,21 +1,24 @@
 <?php require_once('../../private/initialize.php'); ?>
 
 <?php $page_title = 'Staff Menu'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
 
+	<div>
+	    <?php echo 'SHARED_PATH: ' . SHARED_PATH ?>
+	</div>
 	<div id="content">
 		<div id="main-menu">
 			<h2>Main Menu</h2>
 			<ul>
 				<li>
-					<a href="<?php echo url_for('/staff/subjects/index.php'); ?>">Subjects</a>
+					<a href="<?php echo ''; /*url_for('/staff/subjects/index.php');*/ ?>">Subjects</a>
 				</li>
 				<li>
-					<a href="<?php echo url_for('/staff/pages/index.php'); ?>">Pages</a>
+					<a href="<?php echo ''; /*url_for('/staff/pages/index.php');*/ ?>">Pages</a>
 				</li>
 			</ul>
 		</div>
 	</div>
 
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+<?php include(SHARED_PATH . '/footer.php'); ?>
 
