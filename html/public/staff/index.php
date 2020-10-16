be42fd660fd0aabb1b4d002e9dc098b047f1e8f6
Steven.Gomez <steve.gomez@biomerieux.com>
2020-10-16 16:57:38 -0500

    html/public/staff/index.php now works

--- html/public/staff/index.php
+++ html/public/staff/index.php
@@ -3,19 +3,23 @@
 <?php $page_title = 'Staff Menu'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
+    <hr />
 	<div>
-	    <?php echo 'SHARED_PATH: ' . SHARED_PATH ?>
+        <?php echo 'WWW_ROOT: ' . WWW_ROOT ?><br />
+        <?php echo url_for('stylesheets/staff.css'); ?><br />
+        <?php echo 'SHARED_PATH: ' . SHARED_PATH ?><br />
 	</div>
     <hr />
+
 	<div id="content">
 		<div id="main-menu">
 			<h2>Main Menu</h2>
 			<ul>
 				<li>
-					<a href="<?php echo ''; /*url_for('/staff/subjects/index.php');*/ ?>">Subjects</a>
+                <a href="<?php echo url_for('/staff/subjects/index.php'); ?>">Subjects</a>
 				</li>
 				<li>
-					<a href="<?php echo ''; /*url_for('/staff/pages/index.php');*/ ?>">Pages</a>
+                <a href="<?php echo url_for('/staff/pages/index.php'); ?>">Pages</a>
 				</li>
 			</ul>
 		</div>
