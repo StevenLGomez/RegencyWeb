411e0b8d60a500737017b35e253864d2a5225be4
Steven.Gomez <steve.gomez@biomerieux.com>
2020-10-28 17:14:02 -0500

    Adding some diagnostic code to troubleshoot single page submission

--- html/public/staff/owners/index.php
+++ html/public/staff/owners/index.php
@@ -1,9 +1,36 @@
 
-<?php require_once('../../../private/initialize.php'); ?>
+<?php 
+    require_once('../../../private/initialize.php');
 
-<!-- Queries used on this page -->
-<?php $lot_set = find_all_lots(); ?>
-<?php $owner_set = find_all_owners(); ?>
+    $getid = 'No';
+
+    if (isset($_GET['id']))
+    {
+        $getid = 'Yes';
+    }
+
+    $is_post = 'Not sure';
+    if (is_post_request())
+    {
+        $is_post = 'Yes sir';
+    }
+    else
+    {
+        $is_post = 'Nope';
+    }
+
+    if (isset($_POST['submit']))
+    {
+        $is_submit = 'Yes';
+    }
+    else
+    {
+        $is_submit = 'No';
+    }
+
+    $lot_set = find_all_lots();
+
+?>
 
 <!-- Assign page title & include header -->
 <?php $page_title = 'Manage Owners'; ?>
@@ -12,7 +39,11 @@
     <div id="content">
         <div id="regency-menu">
             <h2>Owner History Management</h2>
-
+            <hr />
+                <?php echo 'Get ID :' . $getid . '<br />'; ?>
+                <?php echo 'Post request :' . $is_post . '<br />'; ?>
+                <?php echo 'id :' . $id . '<br />'; ?>
+                <?php echo 'Is Submit :' . $is_submit . '<br />'; ?>
             <hr />
             <div class="actions"> 
                 <a class="action" href="<?php echo url_for('/staff/categories/index.php'); ?>">Create New Owner</a>
@@ -20,35 +51,28 @@
             <hr />
             <h3>Search Criteria</h3>
 
-            <!-- The ADDRESS pull down select item -->
-            <div class="actions"> 
-                <label for "lot">By Address</label>
-                <select name="lot" id="lot">
+            <form action="" method="post">
 
-                    <?php while($lot = mysqli_fetch_assoc($lot_set)) { ?>
-                        <option value="<?php htmlsc($lot['id']); ?>"><?php echo htmlsc($lot['address']); ?></option>
-                    <?php } ?>
+                <!-- The ADDRESS pull down select item -->
+                <div class="actions"> 
+                    <label for "lot">By Address</label>
+                    <select name="lot" id="lot">
 
-                </select>
-            </div>
-            <hr />
-            <?php mysqli_free_result($lot_set); ?>
-            <!-- END The ADDRESS pull down select item -->
+                        <?php while($lot = mysqli_fetch_assoc($lot_set)) { ?>
+                            <option value="<?php htmlsc($lot['id']); ?>"><?php echo htmlsc($lot['address']); ?></option>
+                        <?php } ?>
 
-            <!-- The Owner name pull down select item -->
-            <div class="actions"> 
-                <label for "owner">By Last Name</label>
-                <select name="owner" id="owner">
+                    </select>
+                </div>
+                <hr />
+                <?php mysqli_free_result($lot_set); ?>
+                <!-- END The ADDRESS pull down select item -->
 
-                    <?php while($owner = mysqli_fetch_assoc($owner_set)) { ?>
-                        <option value="<?php htmlsc($owner['id']); ?>"><?php echo htmlsc($owner['last']); ?></option>
-                    <?php } ?>
+                <div id="operations">
+                    <input type="submit" name="submit" value="Do It" />
+                </div>
 
-                </select>
-            </div>
-            <hr />
-            <?php mysqli_free_result($owner_set); ?>
-            <!-- END The Owner name pull down select item -->
+            </form>
 
 
 
