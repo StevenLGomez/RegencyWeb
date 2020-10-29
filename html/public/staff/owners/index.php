505b5a2c904ba5e154f8aaf5275097ad6dffa11a
Steven.Gomez <steve.gomez@biomerieux.com>
2020-10-29 12:37:25 -0500

    Snapshot for  experiments

--- html/public/staff/owners/index.php
+++ html/public/staff/owners/index.php
@@ -2,37 +2,34 @@
 <?php 
     require_once('../../../private/initialize.php');
 
-    $getid = 'No';
+    $is_submit = 'No';
+    $has_lot = 'No';
 
-    if (isset($_GET['id']))
-    {
-        $getid = 'Yes';
-    }
-
-    $is_post = 'Not sure';
     if (is_post_request())
     {
-        $is_post = 'Yes sir';
-    }
-    else
-    {
-        $is_post = 'Nope';
-    }
+        if (isset($_POST['submit']))
+        {
+            $is_submit = 'Yes';
+        }
 
-    if (isset($_POST['submit']))
-    {
-        $is_submit = 'Yes';
+        if (isset($_POST['lot']))
+        {
+            $lot_id = $_POST['lot'];
+            $has_lot = 'Yes';
+        }
+        else
+        {
+        }
     }
     else
     {
-        $is_submit = 'No';
     }
 
     $lot_set = find_all_lots();
 
 ?>
 
-<!-- Assign page title & include header -->
+<!-- Assign page title (used in header) & include header -->
 <?php $page_title = 'Manage Owners'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
@@ -40,15 +37,18 @@
         <div id="regency-menu">
             <h2>Owner History Management</h2>
             <hr />
-                <?php echo 'Get ID :' . $getid . '<br />'; ?>
-                <?php echo 'Post request :' . $is_post . '<br />'; ?>
-                <?php echo 'id :' . $id . '<br />'; ?>
                 <?php echo 'Is Submit :' . $is_submit . '<br />'; ?>
+                <?php echo 'Post variables: ' . $_POST['lot'] . '<br />'; ?>
+                <?php echo 'has_lot :' . $has_lot . '<br />'; ?>
+                <?php echo 'lot_id :' . $lot_id . '<br />'; ?>
             <hr />
+
+            <!-- This div is a placeholder, not expected to work properly at the moment -->
             <div class="actions"> 
                 <a class="action" href="<?php echo url_for('/staff/categories/index.php'); ?>">Create New Owner</a>
             </div>
             <hr />
+
             <h3>Search Criteria</h3>
 
             <form action="" method="post">
@@ -69,7 +69,7 @@
                 <!-- END The ADDRESS pull down select item -->
 
                 <div id="operations">
-                    <input type="submit" name="submit" value="Do It" />
+                    <input type="submit" name="submit" value="Make it so..." />
                 </div>
 
             </form>
