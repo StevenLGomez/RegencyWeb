89e7900d773a82b2f7f904217dd8699063771b5e
Steven Gomez <steve_gomez@usa.net>
2021-07-24 11:43:58 -0500

    Converted table framework into PHP using previous examples

--- html/public/staff/owners/create.php
+++ html/public/staff/owners/create.php
@@ -1,21 +1,20 @@
 <?php 
     require_once('../../../private/initialize.php');
-
-    // Set defaults for local variables
-    $lot_is_selected = False;
-    $lot_id = '';
-
-    if (is_post_request())
-    {
-        if (isset($_POST['address_id']))
-        {
-            $lot_id = $_POST['address_id'];
-            $lot_is_selected = True;
-        }
-    }
-
-    $lot_query = find_all_lots();
 ?>
+
+<!-- Show diagnostic information -->
+<?php
+if ( $diagnostics_enabled) {
+   echo '<hr />';
+   echo '<div>';
+
+   echo 'Diagnostics placeholder';
+
+   echo '</div>';
+   echo '<hr />';
+   } ?>
+<!-- *************************** -->
+
 
     <!-- Assign page title (used in header) & include header -->
     <?php $page_title = 'Create New Owner'; ?>
@@ -25,41 +24,83 @@
             <div id="regency-menu">
                 <h2>Create New Owner</h2>
 
-                <!-- Show diagnostic information -->
-                   <?php
-                   if ( $diagnostics_enabled) {
-                       echo '<hr />';
-                       echo '<div>';
+            <hr />
+            <h3>Primary Owner</h3>
+            <form action="<?php echo ''; ?>" method="post">
+                <dl>
+                    <dt>First</dt>
+                    <dd><input type="text" name ="menu_name" value="<?php echo 'Abraham'; ?>" /> </dd>
+                </dl>
+                <dl>
+                    <dt>Middle</dt>
+                    <dd><input type="text" name ="menu_name" value="<?php echo 'Bob'; ?>" /> </dd>
+                </dl>
+                <dl>
+                    <dt>Last</dt>
+                    <dd><input type="text" name ="menu_name" value="<?php echo 'Lincoln'; ?>" /> </dd>
+                </dl>
 
-                       echo 'Diagnostics placeholder';
+            <h3>Secondary Owner</h3>
+                <dl>
+                    <dt>First</dt>
+                    <dd><input type="text" name ="menu_name" value="<?php echo 'Amelia'; ?>" /> </dd>
+                </dl>
+                <dl>
+                    <dt>Middle</dt>
+                    <dd><input type="text" name ="menu_name" value="<?php echo 'J'; ?>" /> </dd>
+                </dl>
+                <dl>
+                    <dt>Last</dt>
+                    <dd><input type="text" name ="menu_name" value="<?php echo 'Earhardt'; ?>" /> </dd>
+                </dl>
 
-                       echo '</div>';
-                       echo '<hr />';
-                       } ?>
+            <hr />
+            <dl>
+                <dt>Closing Date</dt>
+                <dd><input type="text" name ="menu_name" value="<?php echo '20210722'; ?>" /> </dd>
+            </dl>
 
-            <table class="list">
-                <tr>
-                    <th></th>
-                    <th>Primary Owner</th>
-                    <th>Secondary</th>
-                </tr>
-                <tr>
-                    <td>First</td>
-                    <td>Abraham</td>
-                    <td>Amelia</td>
-                </tr>
-                <tr>
-                   <td>Middle</td>
-                   <td>Joe</td>
-                   <td></td>
-                </tr>
-               <tr>
-                  <td>Last</td>
-                  <td>Lincoln</td>
-                  <td>Earhardt</td>
-               </tr>
+            <dl>
+                <dt>Lot ID</dt>
+                <dd><input type="integer" name ="menu_name" value="<?php echo '25'; ?>" /> </dd>
+            </dl>
 
-            </table>
+            <dl>
+                <dt>Current Owner?</dt>
+                <dd><input type="checkbox" name ="menu_name" value="checked" /> </dd>
+            </dl>
+
+            <hr />
+            <h3>Address</h3>
+            <dl>
+                <dt>Same as lot?</dt>
+                <dd><input type="checkbox" name ="menu_name" value="checked" /> </dd>
+            </dl>
+
+            <dl>
+                <dt>City</dt>
+                <dd><input type="text" name ="menu_name" value="<?php echo 'Saint Peters'; ?>" /> </dd>
+            </dl>
+
+            <dl>
+                <dt>State</dt>
+                <dd><input type="text" name ="menu_name" value="<?php echo 'Missouri'; ?>" /> </dd>
+            </dl>
+
+            <dl>
+                <dt>Zip</dt>
+                <dd><input type="text" name ="menu_name" value="<?php echo '63303'; ?>" /> </dd>
+            </dl>
+
+            <div id="operations">
+                <input type="submit" value="Create New Owner" />
+            </div>
+
+            </form>
+
+
+        </div>
+    </div>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
 
