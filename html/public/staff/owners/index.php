a361a292dcfa6644c77e0d623d799ca423c84178
Steven Gomez <steve_gomez@usa.net>
2022-04-09 17:44:13 -0500

    Search by last name now functional

--- html/public/staff/owners/index.php
+++ html/public/staff/owners/index.php
@@ -76,13 +76,12 @@
             <!-- Provide link to enter new Owner information -->
             <div class="actions"> 
                 <a class="action" href="<?php echo url_for('/staff/owners/create.php'); ?>">Create New Owner</a><br />
-                <a class="action" href="<?php echo url_for('/staff/owners/show.php'); ?>">Show Single Owner</a>
             </div>
             <hr />
 
-            <h3>Search By Lot Address</h3>
+            <h3>Search By Address</h3>
 
-            <!-- Form for Searching by Lot Address -->
+            <!-- Form for Searching by Address -->
             <form action="" method="post">
 
                 <!-- The ADDRESS pull down select item -->
@@ -103,7 +102,7 @@
                     <input type="submit" name="submit" value="Search by Address" />
                 </div>
             </form>
-            <!-- ************************************************************ -->
+            <!-- ============================================================ -->
 
             <!-- Form for Searching by Last name -->
             <hr />
@@ -122,7 +121,7 @@
                     <input type="submit" name="submit" value="Search by Last" />
                 </div>
             </form>
-            <!-- ************************************************************ -->
+            <!-- ============================================================ -->
 
             <!-- ************************************************************ -->
             <!-- This section only entered after an Address has been selected -->
@@ -160,7 +159,7 @@
                   <?php mysqli_free_result($owner_query); ?>
 
             <?php } /* if ($lot_is_selected) */  ?>
-            <!-- ************************************************************ -->
+            <!-- ============================================================ -->
 
             <!-- ************************************************************ -->
             <!-- This section only entered after a Last Name has been entered -->
@@ -174,30 +173,111 @@
 
                   <table class="list">
                       <tr>
-                          <th>First</th>
-                          <th>MI</th>
-                          <th>Last</th>
-                          <th>Address</th>
-                          <th>City</th>
-                          <th>State</th>
-                          <th>Zip</th>
-                          <th>Current</th>
+                          <th>-----</th>
+                          <th>Primary Owner</th>
+                          <th>Secondary Owner</th>
                       </tr>
 
+                    <?php $owner = mysqli_fetch_assoc($owner_query); ?>
 
+                      <tr>
+                          <td>First Name</td>
+                          <td><?php echo htmlsc($owner['first']); ?>    </td>
+                          <td><?php echo htmlsc($owner['first_2']); ?>    </td>
+                      </tr>
 
+                      <tr>
+                          <td>Middle</td>
+                          <td><?php echo htmlsc($owner['mi']); ?>    </td>
+                          <td><?php echo htmlsc($owner['mi_2']); ?>    </td>
+                      </tr>
 
+                      <tr>
+                          <td>Last Name</td>
+                          <td><?php echo htmlsc($owner['last']); ?>    </td>
+                          <td><?php echo htmlsc($owner['last_2']); ?>    </td>
+                      </tr>
 
+                      <tr>
+                          <td>Phone</td>
+                          <td><?php echo htmlsc($owner['phone']); ?>    </td>
+                          <td><?php echo htmlsc($owner['phone_2']); ?>    </td>
+                      </tr>
 
+                      <tr>
+                          <td>Email</td>
+                          <td><?php echo htmlsc($owner['email']); ?>    </td>
+                          <td><?php echo htmlsc($owner['email_2']); ?>    </td>
+                      </tr>
 
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
 
+                      <tr>
+                          <td>Lot #</td>
+                          <td><?php echo htmlsc($owner['fk_lot_id']); ?>    </td>
+                          <td>-</td>
+                      </tr>
 
+                      <tr>
+                          <td>Buy Date</td>
+                          <td><?php echo htmlsc($owner['buy_date']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Current Owner</td>
+                          <td><?php echo $owner['is_current'] == 1 ? 'Yes' : 'No'; ?></td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Is Rental</td>
+                          <td>TBD</td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Address</td>
+                          <td><?php echo htmlsc($owner['address']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>City</td>
+                          <td><?php echo htmlsc($owner['city']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>State</td>
+                          <td><?php echo htmlsc($owner['state']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Zip</td>
+                          <td><?php echo htmlsc($owner['zip']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Notes</td>
+                          <td>TBD</td>
+                      </tr>
 
                   </table>
 
                   <?php mysqli_free_result($owner_query); ?>
 
             <?php } /* if ($last_name_was_entered) */  ?>
+            <!-- ============================================================ -->
+
+            <br />
+            <br />
 
         </div>
     </div>
