7ffee27e884267514583bd95b7390060742c6f49
Steven Gomez <steve_gomez@usa.net>
2022-04-10 17:03:03 -0500

    Updated Create page to match index.php layout

--- html/public/staff/owners/create.php
+++ html/public/staff/owners/create.php
@@ -1,5 +1,7 @@
 <?php 
     require_once('../../../private/initialize.php');
+
+    $last_name_was_entered = True;
 ?>
 
 <!-- Show diagnostic information -->
@@ -19,100 +21,134 @@ if ( $diagnostics_enabled) {
     <?php $page_title = 'Create New Owner'; ?>
     <?php include(SHARED_PATH . '/header.php'); ?>
 
-        <div id="content">
-            <div id="regency-menu">
-                <h2>Create New Owner</h2>
+    <div id="content">
+        <div id="regency-menu">
+           <h2>Create New Owner</h2>
 
             <hr />
-            <h3>Primary Owner</h3>
-            <form action="<?php echo ''; ?>" method="post">
-                <dl>
-                    <dt>First</dt>
-                    <dd><input type="text" name="first" value="<?php echo 'Abraham'; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>Middle</dt>
-                    <dd><input type="text" name="mi" value="<?php echo 'Bob'; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>Last</dt>
-                    <dd><input type="text" name="last" value="<?php echo 'Lincoln'; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>Phone</dt>
-                    <dd><input type="text" name="phone" value="<?php echo ''; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>email</dt>
-                    <dd><input type="text" name="email" value="<?php echo ''; ?>" /> </dd>
-                </dl>
 
-            <h3>Secondary Owner</h3>
-                <dl>
-                    <dt>First</dt>
-                    <dd><input type="text" name="first_2" value="<?php echo 'Amelia'; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>Middle</dt>
-                    <dd><input type="text" name="mi_2" value="<?php echo 'J'; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>Last</dt>
-                    <dd><input type="text" name="last_2" value="<?php echo 'Earhardt'; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>Phone</dt>
-                    <dd><input type="text" name="phone_2" value="<?php echo ''; ?>" /> </dd>
-                </dl>
-                <dl>
-                    <dt>email</dt>
-                    <dd><input type="text" name="email_2" value="<?php echo ''; ?>" /> </dd>
-                </dl>
+            <!-- ************************************************************ -->
+            <!-- This section only entered after a Last Name has been entered -->
+            <!-- ************************************************************ -->
+            <?php if ($last_name_was_entered) { ?>
+
+            <?php $owner_query = find_owners_by_last('Emerson'); ?>
+               <hr />
+               <?php echo '<h3> Search Results for: ' . $last_name . '</h3>'; ?>
+
+
+                  <table class="list">
+                      <tr>
+                          <th>-----</th>
+                          <th>Primary Owner</th>
+                          <th>Secondary Owner</th>
+                      </tr>
+
+                    <?php $owner = mysqli_fetch_assoc($owner_query); ?>
+
+                      <tr>
+                          <td>First Name</td>
+                          <td><?php echo htmlsc($owner['first']); ?>    </td>
+                          <td><?php echo htmlsc($owner['first_2']); ?>    </td>
+                      </tr>
+
+                      <tr>
+                          <td>Middle</td>
+                          <td><?php echo htmlsc($owner['mi']); ?>    </td>
+                          <td><?php echo htmlsc($owner['mi_2']); ?>    </td>
+                      </tr>
+
+                      <tr>
+                          <td>Last Name</td>
+                          <td><?php echo htmlsc($owner['last']); ?>    </td>
+                          <td><?php echo htmlsc($owner['last_2']); ?>    </td>
+                      </tr>
+
+                      <tr>
+                          <td>Phone</td>
+                          <td><?php echo htmlsc($owner['phone']); ?>    </td>
+                          <td><?php echo htmlsc($owner['phone_2']); ?>    </td>
+                      </tr>
+
+                      <tr>
+                          <td>Email</td>
+                          <td><?php echo htmlsc($owner['email']); ?>    </td>
+                          <td><?php echo htmlsc($owner['email_2']); ?>    </td>
+                      </tr>
+
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+
+                      <tr>
+                          <td>Lot #</td>
+                          <td><?php echo htmlsc($owner['fk_lot_id']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Buy Date</td>
+                          <td><?php echo htmlsc($owner['buy_date']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Current Owner</td>
+                          <td><?php echo $owner['is_current'] == 1 ? 'Yes' : 'No'; ?></td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Is Rental</td>
+                          <td>TBD</td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Address</td>
+                          <td><?php echo htmlsc($owner['address']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>City</td>
+                          <td><?php echo htmlsc($owner['city']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>State</td>
+                          <td><?php echo htmlsc($owner['state']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Zip</td>
+                          <td><?php echo htmlsc($owner['zip']); ?>    </td>
+                          <td>-</td>
+                      </tr>
+
+                      <tr>
+                          <td>Notes</td>
+                          <td>TBD</td>
+                      </tr>
+
+                  </table>
+
+                  <?php mysqli_free_result($owner_query); ?>
+
+            <?php } /* if ($last_name_was_entered) */  ?>
+            <!-- ============================================================ -->
+
+
+
+
+
+
 
             <hr />
-            <dl>
-                <dt>Closing Date</dt>
-                <dd><input type="text" name="buy_date" value="<?php echo '2021-07-22'; ?>" /> </dd>
-            </dl>
-
-            <dl>
-                <dt>Lot ID</dt>
-                <dd><input type="integer" name="fk_lot_id" value="<?php echo '25'; ?>" /> </dd>
-            </dl>
-
-            <dl>
-                <dt>Current Owner?</dt>
-                <dd><input type="checkbox" name="is_current" value="checked" /> </dd>
-            </dl>
-
-            <hr />
-            <h3>Address</h3>
-            <dl>
-                <dt>Same as lot?</dt>
-                <dd><input type="checkbox" name="same_as_lot" value="checked" /> </dd>
-            </dl>
-
-            <dl>
-                <dt>City</dt>
-                <dd><input type="text" name="city" value="<?php echo 'Saint Peters'; ?>" /> </dd>
-            </dl>
-
-            <dl>
-                <dt>State</dt>
-                <dd><input type="text" name="state" value="<?php echo 'Missouri'; ?>" /> </dd>
-            </dl>
-
-            <dl>
-                <dt>Zip</dt>
-                <dd><input type="text" name="zip" value="<?php echo '63303'; ?>" /> </dd>
-            </dl>
-
-            <div id="operations">
-                <input type="submit" value="Create New Owner" />
-            </div>
-
-            </form>
-
 
         </div>
     </div>
