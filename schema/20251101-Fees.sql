0188487fdcdd8a6dfc22cf1e4d1d7ce2584dfc39
Steven Gomez <steve_gomez@usa.net>
2025-11-24 12:40:02 -0600

    Added deposit 163

--- schema/20251101-Fees.sql
+++ schema/20251101-Fees.sql
@@ -55,3 +55,16 @@ INSERT INTO fees(dt, ck_no, amount, fk_lot_id, fk_deposit_id, payee, note) VALUE
 
 UPDATE deposit SET amount = (SELECT SUM(amount) FROM fees WHERE fk_deposit_id = 162) WHERE id = 162;
 
+-- 2025-11-24 Deposit 163
+INSERT INTO deposit (id, dt, is_reconciled) VALUES(163, '2025-11-24', 0); -- Deposit 163
+
+INSERT INTO fees(dt, ck_no, amount, fk_lot_id, fk_deposit_id, payee, note) VALUES('2025-11-16',   1027, 130,  9, 163, 'Metcalfe',  '');
+INSERT INTO fees(dt, ck_no, amount, fk_lot_id, fk_deposit_id, payee, note) VALUES('2025-11-19',    274,  50, 48, 163, 'Vollmer',  '');
+INSERT INTO fees(dt, ck_no, amount, fk_lot_id, fk_deposit_id, payee, note) VALUES('2025-11-24',     58,  50, 54, 163, 'Weaver',  '');
+INSERT INTO fees(dt, ck_no, amount, fk_lot_id, fk_deposit_id, payee, note) VALUES('2025-11-25',   3107,  50, 63, 163, 'White',  '');
+
+UPDATE deposit SET amount = (SELECT SUM(amount) FROM fees WHERE fk_deposit_id = 163) WHERE id = 163;
+
+
+
+
